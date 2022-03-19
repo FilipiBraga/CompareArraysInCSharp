48c7f904cc1aab6f5ae5903c49c4e8c215b39774
filipibc <filipibc@gmail.com>
2022-03-19 15:37:59 -0300

    - MainMethod implementation - Tests implementation - Renaming tests class

--- src/CompareArrays/CompareArrays/ArrayComparer.cs
+++ src/CompareArrays/CompareArrays/ArrayComparer.cs
@@ -1,19 +1,70 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Collections;
 
 namespace CompareArrays
 {
     public class ArrayComparer
     {
-        [Benchmark]
-        public bool EqualOperator(int[] firstArray, int[] secondArray)
+        public bool EqualityOperator(int[] firstArray, int[] secondArray)
         {
             return firstArray == secondArray;
         }
 
-        [Benchmark]
         public bool StringJoin(int[] firstArray, int[] secondArray)
         {
             return string.Join("=>", firstArray) == string.Join("=>", secondArray);
+        }
+
+        public bool ForLoop(int[] firstArray, int[] secondArray)
+        {
+            if (firstArray.Length != secondArray.Length)
+                return false;
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool ForeachLoop(int[] firstArray, int[] secondArray)
+        {
+            if (firstArray.Length != secondArray.Length)
+                return false;
+
+            var index = 0;
+            foreach (var item in firstArray)
+            {
+                if (item != secondArray[index])
+                    return false;
+
+                index++;
+            }
+
+            return true;
+        }
+
+        public bool EnumerableSequenceEqual(int[] firstArray, int[] secondArray)
+        {
+            return Enumerable.SequenceEqual(firstArray, secondArray);
+        }
+
+        public bool EnumerableEquals(int[] firstArray, int[] secondArray)
+        {
+            return Enumerable.Equals(firstArray, secondArray);
+        }
+
+        public bool EnumerableReferenceEquals(int[] firstArray, int[] secondArray)
+        {
+            return Enumerable.ReferenceEquals(firstArray, secondArray);
+        }
+
+        public bool StructuralEquatable(int[] firstArray, int[] secondArray)
+        {
+            IStructuralEquatable structuralEquatable = firstArray;
+
+            return structuralEquatable.Equals(secondArray, StructuralComparisons.StructuralEqualityComparer);
         }
     }
 }
--- src/CompareArrays/CompareArrays/Program.cs
+++ src/CompareArrays/CompareArrays/Program.cs
@@ -1,6 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BenchmarkDotNet.Running;
-using CompareArrays;
+﻿namespace CompareArrays
+{
+    public class Program
+    {
+        private static readonly ArrayComparer _arrayComparer = new();
+        private static readonly int[] _firstArray = new int[] { 10, 9, 3, 8, 7 };
+        private static readonly int[] _secondArray = new int[] { 10, 9, 3, 8, 7 };
+        public static int OutPutResult = 0;
 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---------- Compare Arrays in C#");
+            Console.WriteLine("-------------------------------");
 
-BenchmarkRunner.Run<ArrayComparer>();
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Compare using Equality operator Operator (==)");
+            Console.WriteLine($"Are equal: {_arrayComparer.EqualityOperator(_firstArray, _secondArray)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Compare using String.Join");
+            Console.WriteLine($"Are equal: {_arrayComparer.StringJoin(_firstArray, _secondArray)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Compare using For Loop");
+            Console.WriteLine($"Are equal: {_arrayComparer.ForLoop(_firstArray, _secondArray)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Compare using Forach Loop");
+            Console.WriteLine($"Are equal: {_arrayComparer.ForeachLoop(_firstArray, _secondArray)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Compare using Enumerable.SequenceEqual");
+            Console.WriteLine($"Are equal: {_arrayComparer.EnumerableSequenceEqual(_firstArray, _secondArray)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Compare using Enumerable.Equals");
+            Console.WriteLine($"Are equal: {_arrayComparer.EnumerableEquals(_firstArray, _secondArray)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Compare using Enumerable.ReferenceEquals");
+            Console.WriteLine($"Are equal: {_arrayComparer.EnumerableReferenceEquals(_firstArray, _secondArray)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Compare using StructuralEquatable");
+            Console.WriteLine($"Are equal: {_arrayComparer.StructuralEquatable(_firstArray, _secondArray)}");
+
+            Console.WriteLine();
+            
+            OutPutResult = 1;
+        }
+    }
+}
